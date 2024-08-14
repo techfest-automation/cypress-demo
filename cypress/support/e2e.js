66b616bccc14952dfcee684e655f1c57cb10321e
techfest-automation <sandeepk@suntechnologies.com>
2024-08-14 23:29:47 +0530

    added the reporter

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import 'cypress-mochawesome-reporter/register';
 import './commands'
 require('cypress-xpath');
 
